c8e64780686b2afc00a89f72dc3c3e9f2e6e255e
hunte <besscomdark@gmail.com>
2024-01-19 20:07:59 +0800

    新增腳本

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameObject flag; // Reference to the flag object
+
+    private bool hasFlag = false;
+
+    void Update()
+    {
+        // Player movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+
+        // Player interaction
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (hasFlag)
+            {
+                // Drop the flag
+                flag.transform.parent = null;
+                hasFlag = false;
+            }
+            else
+            {
+                // Check if the player is near the flag
+                float distance = Vector3.Distance(transform.position, flag.transform.position);
+                if (distance < 2.0f)
+                {
+                    // Pick up the flag
+                    flag.transform.parent = transform;
+                    flag.transform.localPosition = new Vector3(0, 2, 0); // Adjust the flag position
+                    hasFlag = true;
+                }
+            }
+        }
+    }
+}
