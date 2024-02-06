8d930cf131a0b08e770f8865babfd392f302dc39
hunte <besscomdark@gmail.com>
2024-02-06 23:17:44 +0800

    調整

---
+++ Assets/Scripts/FlagController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    private bool isPickedUp = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isPickedUp)
+        {
+            // Player picks up the flag
+            transform.parent = other.transform;
+            transform.localPosition = new Vector3(0, 1, 0); // Adjust flag position
+            isPickedUp = true;
+        }
+    }
+
+    public void DropFlag()
+    {
+        // Called when player drops the flag
+        transform.parent = null;
+        isPickedUp = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
